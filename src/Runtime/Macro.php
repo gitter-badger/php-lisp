b2ef2b184cc5de65e7b26d1bb46cc5345ea8992c
wujunze <itwujunze@163.com>
2018-12-11 12:39:17 +0800

    init PhpLisp

---
+++ src/Runtime/Macro.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the php-lisp/php-lisp.
+ *
+ * @Link     https://github.com/php-lisp/php-lisp
+ * @Document https://github.com/php-lisp/php-lisp/blob/master/README.md
+ * @Contact  itwujunze@gmail.com
+ * @License  https://github.com/php-lisp/php-lisp/blob/master/LICENSE
+ *
+ * (c) Panda <itwujunze@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+class Macro
+{
+}
